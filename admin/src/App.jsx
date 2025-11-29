65b343c295061a2a55646dab5be8e45fe1e067ec
Zaid Khan <zaid.s19.khan@gmail.com>
2025-11-29 23:03:39 +0530

    Fix import case for Login component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import Login from './pages/login'
+import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Products from './pages/Products'
 import Categories from './pages/Categories'
